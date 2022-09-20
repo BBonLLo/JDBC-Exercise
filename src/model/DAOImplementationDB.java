5045765af8f7e370524745cee8d3d65b51cb7fcd
2dam <2dam@HZ375757.TartangaLH.eus>
2022-09-20 08:36:25 +0200

    first commit gonna work on DB implementation

--- src/model/DAOImplementationDB.java
+++ src/model/DAOImplementationDB.java
@@ -5,10 +5,55 @@
  */
 package model;
 
+import ExceptionManager.ExceptionManager;
+import clases.Account;
+import clases.Customer;
+import clases.Movement;
+
 /**
  *
- * @author unaib
+ * @author unaiz
  */
-public class DAOImplementationDB {
+public class DAOImplementationDB implements DAO {
+    
+    @Override
+    public void createCustomer(Customer customer) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Customer getCustomerData(Customer customer) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Account getCustomerAccounts(Customer customer) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void createCustomerAccount(Customer customer) throws ExceptionManager {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void addClientToAccount(Customer customer, Account account) throws ExceptionManager {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Account getAccountData(Account account) throws ExceptionManager {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void makeAccountMovement(Account account, Movement movement) throws ExceptionManager {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Account getAccountMovement(Account account) throws ExceptionManager {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
